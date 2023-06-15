7757257dc46361571b46e56fa822a6becfeecde0
Austin Morse <amorse110@gmail.com>
2023-06-15 09:43:43 -0700

    lab complete

--- index.js
+++ index.js
@@ -10,23 +10,24 @@ function moveDodgerLeft() {
   }
 }
 
+//MOVE RIGHT
+function moveDodgerRight() {
+  const rightNumbers = dodger.style.left.replace("px", "");
+  const right = parseInt(rightNumbers, 10);
+
+  if (right < 360) {
+    dodger.style.left = `${right + 1}px`;
+  }
+}
+
 document.addEventListener("keydown", function (e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
+if (e.key === "ArrowLeft") {
+  moveDodgerLeft();
   }
 });
 
-//MOVE RIGHT
-function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-
-    if (left < 360) {
-        dodger.style.left = `${left + 1}px`;
-    }
-}
-document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowRight") {
-        moveDodgerRight();
-    }
-})
+document.addEventListener('keydown', function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
